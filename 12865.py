dc8d78b1ed5c90c0be7a3a521b953df75a2cff52
이명진 <88179341+thingineeer@users.noreply.github.com>
2023-09-16 23:47:58 +0900

    평범한 배낭
    Knapsack 아닌 DP 로 해결

---
+++ 12865.py
@@ -0,0 +1,10 @@
+import sys
+input = sys.stdin.readline
+
+N, K = map(int, input().rstrip().split())
+dp = [0] * (K+1)
+for _ in range(N):
+    W, V = map(int, input().rstrip().split())
+    for i in range(K, W-1, -1):
+        dp[i] = max(dp[i], dp[i-W] + V)
+print(dp[K])
